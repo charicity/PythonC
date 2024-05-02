1306a1191f5089233f6af3efcc4ed282cd7c6f73
charicity <gene_work@163.com>
2024-05-02 12:47:44 +0800

    吃个饭先， set 还没 debug 完

--- src/setPyobj.hpp
+++ src/setPyobj.hpp
@@ -9,10 +9,10 @@
     fprintf(stderr, s, __FUNCTION__); \
     PyErr_Format(PyExc_RuntimeError, s, __FUNCTION__)
 
-void setFromPyObj(PyObject* object, context_value* context);
+bool setFromPyObj(PyObject* object, context_value* context);
 
 static bool isBasicType(PyObject* object) {
-    PyTypeObject* type = Py_TYPE(object);
+    PyTypeObject* type = Py_TYPE(object);  // 不需要处理
     if (type == &PyBool_Type) {
         return true;
     } else if (type == &PyLong_Type) {
@@ -29,16 +29,21 @@ static bool isBasicType(PyObject* object) {
         return true;
     } else if (type == &PyTuple_Type) {
         return true;
+    } else if (type == &_PyNone_Type) {
+        fprintf(stderr, "[Warning] Setting none type.");
+        return true;
     }
     return false;
 }
 
-static void setPyobjByBool(PyObject* object, context_value* site) {
-    long val = PyLong_AsLong(object);
+/*以下的这个 setPyobjBy'xxtype' 都是经过分流的，不需要额外判断类型*/
+static bool setPyobjByBool(PyObject* object, context_value* site) {
+    bool val = static_cast<bool>(PyLong_AsLong(object));  // hummm
     site->set_bool_(val);
+    return true;
 }
 
-static void setPyobjByLong(PyObject* object, context_value* site) {
+static bool setPyobjByLong(PyObject* object, context_value* site) {
     // 可以继续优化数据的类型
     int overflow;
 
@@ -46,54 +51,72 @@ static void setPyobjByLong(PyObject* object, context_value* site) {
         long long val = PyLong_AsLongLongAndOverflow(object, &overflow);
         if (!overflow) {
             site->set_int64(val);
-            return;
+            return false;
         }
     }
 
     {
         unsigned long long val = PyLong_AsUnsignedLongLong(object);
+        if (PyErr_ExceptionMatches(PyExc_OverflowError)) {
+            fprintf(stderr, "[Error] PyLong type overflowed in %s().\n",
+                    __FUNCTION__);
+            return false;
+        }
         site->set_uint64(val);
-        return;
     }
-
-    PyErr_SetString(PyExc_RuntimeError, "Integer too long!");
+    return true;
 }
 
-static void setPyobjByBytes(PyObject* object, context_value* site) {
+static bool setPyobjByBytes(PyObject* object, context_value* site) {
     auto val = PyBytes_AsString(object);
+    // 文档没说别的异常...
     site->set_bytes(val);
+    return true;
 }
 
-static void setPyobjByString(PyObject* object, context_value* site) {
+static bool setPyobjByString(PyObject* object, context_value* site) {
     auto val = PyUnicode_AsUTF8(object);
+    // 文档没说别的异常...
     site->set_string(val);
+    return true;
 }
 
-static void setPyobjByFloat(PyObject* object, context_value* site) {
+static bool setPyobjByFloat(PyObject* object, context_value* site) {
     auto val = PyFloat_AsDouble(object);
+    // 限制了 float 类型，所以不用异常处理
     site->set_double_(val);
+    return true;
 }
 
-static void setPyobjByList(PyObject* object, context_value* site) {
+static bool setPyobjByList(PyObject* object, context_value* site) {
     ArrayValue* tmp = new ArrayValue();
+    if (tmp == nullptr) {
+        setErrorCombo("[Error] Failed to malloc for ArrayValue in %s().\n");
+        return false;
+    }
     Py_ssize_t size = PyList_Size(object);
 
     for (Py_ssize_t i = 0; i < size; ++i) {
-        PyObject* item = PyList_GetItem(object, i);  // 窃取引用
+        PyObject* item = PyList_GetItem(object, i);  // 借入引用
 
         if (!isBasicType(item)) {
             delete tmp;
-            PyErr_SetString(PyExc_TypeError, "List contains not basical value");
-            return;
+            setErrorCombo("[Error] List contains non-basic values in %s().\n");
+            return false;
         }
 
         auto ptr = tmp->add_data();
-        setFromPyObj(item, ptr);
+        if (!setFromPyObj(item, ptr)) {
+            setErrorCombo("[Error] Failed to set PyObj in %s().\n");
+            return false;
+        }
     }
-    site->set_allocated_array_value(tmp);
+    site->set_allocated_array_value(tmp);  // 指针管理转移
+    tmp = nullptr;
+    return true;
 }
 
-static void setPyobjByDict(PyObject* object, context_value* site) {
+static bool setPyobjByDict(PyObject* object, context_value* site) {
     PyObject *key, *value;
     Py_ssize_t pos = 0;
     MapString* tmp = new MapString();
@@ -102,55 +125,66 @@ static void setPyobjByDict(PyObject* object, context_value* site) {
         if (PyUnicode_Check(key)) {
             std::string keyStr(PyUnicode_AsUTF8(key));
 
-            setFromPyObj(value, &(*tmp->mutable_data())[keyStr]);
+            if (!setFromPyObj(value, &(*tmp->mutable_data())[keyStr])) {
+                setErrorCombo("[Error] Failed to set PyObj in %s().\n");
+                return false;
+            }
         } else {
             delete tmp;
-            PyErr_SetString(PyExc_TypeError, "Dict's key is not str type");
-            return;
+            setErrorCombo("Dict's key is not str type in %s().\n");
+            return false;
         }
     }
     site->set_allocated_map_string(tmp);
+    return true;
 }
 
-static void setPyobjByTuple(PyObject* object, context_value* site) {
+static bool setPyobjByTuple(PyObject* object, context_value* site) {
     ArrayValue* tmp = new ArrayValue();
     Py_ssize_t size = PyTuple_Size(object);
 
     for (Py_ssize_t i = 0; i < size; ++i) {
-        PyObject* item = PyTuple_GetItem(object, i);  // 窃取引用
+        PyObject* item = PyTuple_GetItem(object, i);  // 借入引用
 
         if (!isBasicType(item)) {
             delete tmp;
-            PyErr_SetString(PyExc_TypeError, "List contains not basical value");
-            return;
+            setErrorCombo(
+                "[ERROR] Tuple contains not basical value in %s().\n");
+            return false;
         }
 
         auto ptr = tmp->add_data();
-        setFromPyObj(item, ptr);
+        if (!setFromPyObj(item, ptr)) {
+            delete tmp;
+            setErrorCombo("[Error] Failed to set PyObj in %s().\n");
+            return false;
+        }
     }
     site->set_allocated_array_value(tmp);
+    return true;
 }
 
-void setFromPyObj(PyObject* object, context_value* context) {
+bool setFromPyObj(PyObject* object, context_value* context) {
     PyTypeObject* type = Py_TYPE(object);
     if (type == &PyBool_Type) {
-        setPyobjByBool(object, context);
+        return setPyobjByBool(object, context);
     } else if (type == &PyLong_Type) {
-        setPyobjByLong(object, context);
+        return setPyobjByLong(object, context);
         // 数据类型可以优化
     } else if (type == &PyBytes_Type) {
         // 二进制安全可以加上？
-        setPyobjByBytes(object, context);
+        return setPyobjByBytes(object, context);
     } else if (type == &PyFloat_Type) {
         // 看起来只能变成 double 类型...
-        setPyobjByFloat(object, context);
+        return setPyobjByFloat(object, context);
     } else if (type == &PyUnicode_Type) {
-        setPyobjByString(object, context);
+        return setPyobjByString(object, context);
     } else if (type == &PyList_Type) {
-        setPyobjByList(object, context);
+        return setPyobjByList(object, context);
     } else if (type == &PyDict_Type) {
-        setPyobjByDict(object, context);
+        return setPyobjByDict(object, context);
     } else if (type == &PyTuple_Type) {
-        setPyobjByTuple(object, context);
+        return setPyobjByTuple(object, context);
     }
+    return false;
 }
